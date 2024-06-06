d412ba1ad899e3e55f3562a44145f0164c36625b
Nikhil <nikhilbchandran@gmail.com>
2024-06-06 19:35:35 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
 
 function App() {
   return (
     <div className="App">
-     <h1>haii</h1>
+      <WeatherApp/>
     </div>
   );
 }
--- src/Components/WeatherApp/weatherApp.jsx
+++ src/Components/WeatherApp/weatherApp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './weatherApp.css'
+import './WeatherApp.css'
 
 
 import search_icon from '../Assets/search.png'
@@ -11,88 +11,83 @@ import snow_icon from '../Assets/snow.png'
 import wind_icon from '../Assets/wind.png'
 import humidity_icon from '../Assets/humidity.png'
 
+const WeatherApp = () => {
 
+  const api_key = process.env.REACT_APP_API_KEY
+  
+  const [wicon,setWicon] = useState(cloud_icon)
+  const [cityInput,setCityInput] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  
+  const search = async () => {
 
-const weatherApp = () => {
-    const api_key = process.env.REACT_APP_API_KEY
-    console.log(api_key,"appiiikeyyy")
+    const element = document.getElementsByClassName("cityInput")
+    if(element[0].value === "")
+    {
+      setErrorMessage("Please enter a valid city name");
+      return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+try{
+  let response = await fetch(url)
 
-    const [wicon,setWicon] = useState(cloud_icon)
-    const [cityInput,setCityInput] = useState('')
-    const [errormessage,setErrormessage] = useState('')
+  if(!response.ok){
+    setErrorMessage(`Error: ${response.status} - ${response.statusText}`);
+    return
+  }
+  
+    let data = await response.json();
 
-    const search = async() => {
+    
+    const humidity = document.getElementsByClassName("humidity-percent")
+    const wind = document.getElementsByClassName("wind-rate")
+    const temperature = document.getElementsByClassName("weather-temp")
+    const location = document.getElementsByClassName("weather-location")
+  
+     humidity[0].innerHTML = data.main.humidity +" %"
+     wind[0].innerHTML = Math.floor(data.wind.speed )+" Km/h"
+     temperature[0].innerHTML = Math.floor(data.main.temp)+"°C"
+     location[0].innerHTML = data.name 
 
-        const element = document.getElementsByClassName('cityInput')
-        if(element[0].value === ""){
-            setErrormessage("Please enter a valid city name")
-            return
-        }
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
-        console.log(url,"urllll")
-        try{
-            let response = await fetch(url)
+     if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n" )
+     {
+      setWicon(clear_icon)
+     }
+     else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
+     {
+      setWicon(cloud_icon)
+     }
+     else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
+     {
+      setWicon(drizzle_icon)
+     }
+     else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
+     {
+      setWicon(drizzle_icon)
+     }
+     else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
+     {
+      setWicon(rain_icon)
+     }
+     else if(data.weather[0].icon === "010d" || data.weather[0].icon === "010n")
+     {
+      setWicon(rain_icon)
+     }
+     else if(data.weather[0].icon === "013d" || data.weather[0].icon === "013n")
+     {
+      setWicon(snow_icon)
+     }
+     else
+     {
+      setWicon(clear_icon)
+     }
 
-            if(!response.ok){
-                setErrormessage(`Error: ${response.status} - ${response.statusText}`)
-                return
-            }
-
-            let data = await response.json();
-
-            const humidity = document.getElementsByClassName("humidity-percent")
-            const wind = document.getElementsByClassName("wind-rate")
-            const temperature = document.getElementsByClassName("weather-temp")
-            const location = document.getElementsByClassName('weather-location')
-
-
-            humidity[0].innerHTML = data.main.humidity +" %"
-            wind[0].innerHTML = Math.floor(data.wind.speed )+" Km/h"
-            temperature[0].innerHTML = Math.floor(data.main.temp)+"°C"
-            location[0].innerHTML = data.name 
-
-
-            if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n" )
-                {
-                 setWicon(clear_icon)
-                }
-                else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
-                {
-                 setWicon(cloud_icon)
-                }
-                else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
-                {
-                 setWicon(drizzle_icon)
-                }
-                else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
-                {
-                 setWicon(drizzle_icon)
-                }
-                else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
-                {
-                 setWicon(rain_icon)
-                }
-                else if(data.weather[0].icon === "010d" || data.weather[0].icon === "010n")
-                {
-                 setWicon(rain_icon)
-                }
-                else if(data.weather[0].icon === "013d" || data.weather[0].icon === "013n")
-                {
-                 setWicon(snow_icon)
-                }
-                else
-                {
-                 setWicon(clear_icon)
-                }
-           
-                setErrormessage("")
-               }catch(error){              
-                setErrormessage("An error occurred while fetching the weather data.")
-               }
+     setErrorMessage("")
+    }catch(error){              
+      setErrorMessage("An error occurred while fetching the weather data.")
     }
 
-
-
+  }
 
   return (
     <div className='container'>
@@ -110,7 +105,7 @@ const weatherApp = () => {
       <div className="weather-image">
         <img src={wicon} alt="" />
       </div>
-      <div className="weather-temp">24°c</div>
+      <div className="weather-temp text-3xl font-bold underline">24°c</div>
       <div className="weather-location">London</div>
       <div className="data-container">
         <div className="element">
@@ -128,14 +123,15 @@ const weatherApp = () => {
           </div>
         </div>
       </div>  
-      {errormessage && (
+      {errorMessage && (
           <div>
             <br />
-            <p style={{color:'white'}}>{errormessage}</p>
+            <p style={{color:'white'}}>{errorMessage}</p>
           </div>
         )}
     </div>
   )
 }
 <script src="https://kit.fontawesome.com/b2f61be1e9.js" crossorigin="anonymous"></script>
-export default weatherApp
+
+export default WeatherApp
